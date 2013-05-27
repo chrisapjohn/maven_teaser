38fc9fb5bf42656cdb4951a7134e923aeb68898e
chrisapjohn <chrisupjohn@gmail.com>
2013-05-27 13:41:43 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MavenTeaser::Application.config.secret_token = '8390eac37e444aaabf67676a7c315d3c2215de3c1294100bbf7ab2c7938d22a82bf54b327c9e7ec6adeb5f46adb1fa9bc352b8d2e9ca215218924c87cf0d73bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MavenTeaser::Application.config.session_store :cookie_store, key: '_maven_teaser_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MavenTeaser::Application.config.session_store :active_record_store
