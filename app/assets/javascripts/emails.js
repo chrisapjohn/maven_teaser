2079208597fb83e9b894e48ef55db21c948c9531
Sergey Alexandrov <sergey84718@gmail.com>
2013-07-01 22:12:54 +0400

    success message with the email form

--- app/assets/javascripts/emails.js
+++ app/assets/javascripts/emails.js
@@ -4,6 +4,9 @@
 
 $(document).ready(function(){
 	$('form').submit(function() {  
+	if ($('#success_message').length > 0) {
+		$('#success_message').remove();
+	}
     var valuesToSubmit = $(this).serialize();
     $.ajax({
         url: $(this).attr('action'), //submits it to the given url of the form
@@ -11,7 +14,13 @@ $(document).ready(function(){
         data: valuesToSubmit,
         dataType: "JSON"
     }).success(function(json){
-        $('#success_message').show();
+        //$('#success_message').show();
+		var success_message_html = '<div class="alert alert-success" id="success_message">';
+		success_message_html += 'Success! You will be in touch.';
+		success_message_html += '</div>';
+		
+		$(this).before(success_message_html);	
+		
     });
     return false; 
 	});
