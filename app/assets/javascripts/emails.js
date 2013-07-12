e3173e6c5cebf18bf184a9d790809d9c1439feab
chrisapjohn <chrisupjohn@gmail.com>
2013-07-11 20:05:06 -0400

    Fixed js

--- app/assets/javascripts/emails.js
+++ app/assets/javascripts/emails.js
@@ -3,24 +3,25 @@
 // You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
 $(document).ready(function(){
-	$('form').submit(function() {  
-	if ($('#success_message').length > 0) {
-		$('#success_message').remove();
-	}
+	$('form').submit(function() {
+		$form = $(this);
+		if ($('#success_message').length > 0) {
+			$('#success_message').remove();
+		}
     var valuesToSubmit = $(this).serialize();
     $.ajax({
-        url: $(this).attr('action'), //submits it to the given url of the form
-        type: 'POST',
-        data: valuesToSubmit,
-        dataType: "JSON"
-    }).success(function(json){
-        //$('#success_message').show();
-		var success_message_html = '<div class="alert alert-success" id="success_message">';
-		success_message_html += 'Success! You will be in touch.';
-		success_message_html += '</div>';
-		
-		$(this).before(success_message_html);	
-		
+      url: $(this).attr('action'), //submits it to the given url of the form
+      type: 'POST',
+      data: valuesToSubmit,
+      dataType: "JSON"
+    }).success(function(json) {
+      $('#success_message').show();
+			var success_message_html = '<div class="alert alert-success" id="success_message">';
+			success_message_html += "Success! We'll be in touch.";
+			success_message_html += '</div>';
+			//console.log(this);
+			$form.before(success_message_html);
+			
     });
     return false; 
 	});
